f97e3984ed5bd6a4290ead5919f90d8d9ba917eb
Dennis Snijder <dennissnijder97@gmail.com>
2017-02-11 01:08:12 +0100

    Removed the domain checking as its being handled by the manifest. Added support for git subdomains closes #10

--- content_script.js
+++ content_script.js
@@ -1,6 +1,2 @@
-var host = window.location.host;
-
-if(host == "github.com") {
-    var element = document.getElementsByClassName("header-dark")[0];
-    element.className = element.className.replace(/\header-dark\b/,'');
-}
+var element = document.getElementsByClassName("header-dark")[0];
+element.className = element.className.replace(/\header-dark\b/,'');
