ff6ae93d11cefa517b312278db2dd191d8963185
crecket <crecketgaming@gmail.com>
2017-02-11 01:38:22 +0100

    background script to check for button click, storage permission to sync it to account

--- content_script.js
+++ content_script.js
@@ -1,2 +1,8 @@
 var element = document.getElementsByClassName("header-dark")[0];
-element.className = element.className.replace(/\header-dark\b/,'');
+if (element) {
+    chrome.storage.sync.get(['enabled'], function(results){
+        if(results.enabled){
+            element.className = element.className.replace(/\header-dark\b/, '');
+        }
+    });
+}
