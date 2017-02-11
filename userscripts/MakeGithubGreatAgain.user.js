00d97015b31c697a69f61e9a95a0869ce8bee3eb
Linus Unnebäck <linus@folkdatorn.se>
2017-02-11 12:24:31 +0100

    Use classList API instead of a regex
    While there was nothing wrong with the last approach, this could be viewed as slightly more elegant :)

--- userscripts/MakeGithubGreatAgain.user.js
+++ userscripts/MakeGithubGreatAgain.user.js
@@ -11,6 +11,5 @@
 (function() {
     'use strict';
 
-    var element = document.getElementsByClassName("header-dark")[0];
-    element.className = element.className.replace(/\bheader-dark\b/,'');
+    document.querySelector('.header-dark').classList.remove('header-dark');
 })();
