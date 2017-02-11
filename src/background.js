715f12110da22d11373597ab3b9d23d550b8e6ed
Dennis Snijder <dennissnijder97@gmail.com>
2017-02-11 15:10:30 +0100

    simplified settings swapping

--- src/background.js
+++ src/background.js
@@ -1,14 +1,9 @@
 chrome.browserAction.onClicked.addListener(function (tab) {
     // icon was clicked
     chrome.storage.sync.get(['enabled'], function (results) {
-        // swap value
-        if (results.enabled) {
-            results.enabled = false;
-        } else {
-            results.enabled = true;
-        }
-        // store new setting
-        chrome.storage.sync.set({'enabled': results.enabled});
+
+        // store new swapped setting
+        chrome.storage.sync.set({'enabled': !results.enabled});
 
         // refresh current page
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
