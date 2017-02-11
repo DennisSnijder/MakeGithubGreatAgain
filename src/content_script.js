1af51819b3ee3c786d8a06cf72b33bdc1a19eb9a
crecket <crecketgaming@gmail.com>
2017-02-11 16:28:48 +0100

    dom listeners to modify as soon as possible

--- src/content_script.js
+++ src/content_script.js
@@ -1,12 +1,28 @@
-var element = document.getElementsByClassName("header-dark")[0];
-if (element) {
-    // Default to removing the border
-    element.className = element.className.replace(/\header-dark\b/, '');
+var hasReplaced = false;
 
-    // check storage if we want it back
-    chrome.storage.sync.get(['enabled'], function (results) {
-        if (results.enabled || results.enabled === undefined) {
-            element.className += " header-dark";
+function replaceHeader() {
+    // check if we already replaced the header class
+    if (hasReplaced === false) {
+
+        // check if element exists yet
+        var element = document.getElementsByClassName("header-dark")[0];
+        if (element) {
+            // element exists, store it so we don't run this twice
+            hasReplaced = true;
+
+            // Default to always removing the border
+            element.className = element.className.replace(/\header-dark\b/, '');
+
+            // check storage if we want it back
+            chrome.storage.sync.get(['enabled'], function (results) {
+                if (results.enabled || results.enabled === undefined) {
+                    element.className += " header-dark";
+                }
+            });
         }
-    });
+    }
 }
+
+// Dom event listeners
+document.addEventListener('DOMNodeInserted', replaceHeader);
+document.addEventListener("DOMContentLoaded", replaceHeader);
