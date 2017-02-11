eab0e6505672a8c0293799c4ac6f6c1d8699de0b
Dennis Snijder <dennissnijder97@gmail.com>
2017-02-11 02:03:31 +0100

    Changed the results.enabled checking to check on undefined instead of null

--- src/content_script.js
+++ src/content_script.js
@@ -1,7 +1,8 @@
 var element = document.getElementsByClassName("header-dark")[0];
 if (element) {
     chrome.storage.sync.get(['enabled'], function (results) {
-        if (results.enabled || results.enabled === null) {
+
+        if (results.enabled || results.enabled === undefined) {
             element.className = element.className.replace(/\header-dark\b/, '');
         }
     });
