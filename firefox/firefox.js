c8e68972e6b5c0e3f413003df07da85ef75297db
Flavio Monteiro <flaviomonteiro2013@gmail.com>
2017-03-07 21:40:29 -0300

    Merge chrome and firefox extensions into one

--- firefox/firefox.js
+++
@@ -1,8 +0,0 @@
-function replaceHeader() {
-    var element = document.querySelector('.header-dark');
-    if (element) element.classList.remove('header-dark');
-}
-
-// Dom event listeners
-document.addEventListener('DOMNodeInserted', replaceHeader);
-document.addEventListener('DOMContentLoaded', replaceHeader);
