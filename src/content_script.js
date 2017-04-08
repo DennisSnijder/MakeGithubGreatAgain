84eda30ee17ca26dd4317e23a6479f0d9324be1f
Paul Joey Clark <joey@neuralyte.org>
2017-04-08 15:46:59 +0800

    Refactor: Better naming of update functions

--- src/content_script.js
+++ src/content_script.js
@@ -11,7 +11,7 @@ function initOnce () {
         // send a message to the background script to enable the page action
         chrome.runtime.sendMessage('enable_page_action', function () {});
     }
-    setClasses();
+    applyStyle();
 }
 
 // event listeners
@@ -21,11 +21,26 @@ document.addEventListener('DOMContentLoaded', initOnce);
 // listen for messages from the background script to update
 chrome.runtime.onMessage.addListener(function (message, sender, callback) {
     if (message === 'apply_style') {
-        setClasses();
+        applyStyle();
     }
 });
 
-function clearClasses () {
+function applyStyle () {
+    chrome.storage.sync.get({
+        enabled: true,
+        originalHeader: true,
+        shortHeader: true,
+        shortSearchBox: true,
+        originalColors: true,
+    }, function (options) {
+        resetClasses();
+        if (options.enabled) {
+            setClasses(options);
+        }
+    });
+}
+
+function resetClasses () {
     headerBar.classList.remove('great-header');
     headerBar.classList.add('header-dark');
     headerBar.classList.remove('short-header');
@@ -33,29 +48,18 @@ function clearClasses () {
     document.querySelector('body').classList.remove('great-again');
 }
 
-function setClasses () {
-    chrome.storage.sync.get({
-        enabled: true,
-        originalHeader: true,
-        shortHeader: true,
-        shortSearchBox: true,
-        originalColors: true,
-    }, function (items) {
-        clearClasses();
-        if (items.enabled) {
-            if (items.originalHeader && headerBar) {
-                headerBar.classList.remove('header-dark');
-                headerBar.classList.add('great-header');
-            }
-            if (items.shortHeader && headerBar) {
-                headerBar.classList.add('short-header');
-            }
-            if (items.shortSearchBox && headerBar) {
-                headerBar.classList.add('short-search-box');
-            }
-            if (items.originalColors) {
-                document.querySelector('body').classList.add('great-again');
-            }
-        }
-    });
+function setClasses (options) {
+    if (options.originalHeader && headerBar) {
+        headerBar.classList.remove('header-dark');
+        headerBar.classList.add('great-header');
+    }
+    if (options.shortHeader && headerBar) {
+        headerBar.classList.add('short-header');
+    }
+    if (options.shortSearchBox && headerBar) {
+        headerBar.classList.add('short-search-box');
+    }
+    if (options.originalColors) {
+        document.querySelector('body').classList.add('great-again');
+    }
 }
